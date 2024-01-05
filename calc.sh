06d04f430e7ceef874f740f27a1a6b40e51f8d02
Bastian Bittorf <bb@npl.de>
2024-01-05 09:43:12 +0100

    erster wurf

---
+++ calc.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+#
+# https://www.test.de/steuerprogression-einfach-erklaert-5813257-0/
+# Einkommensteuertabelle 2023 - Grundtabelle
+# https://www.sthu.org/blog/02-bruttonetto/index.html
+# https://www.finanz-tools.de/einkommensteuer/berechnung-formeln/2023
+
+MAX="${1:-120000}"
+STEP="${2:-100}"	# z.b. 25 Euro Schritte
+
+BRUTTO=0
+STEUER=0
+NETTO=0
+
+calc()
+{
+	case "$2" in
+		exact) echo "$1" | bc -l ;;
+		    *) echo "result = ($1); scale=2; result / 1" | bc -l ;;
+	esac
+}
+
+echo "brutto netto steuer effektive-prozentuale-Belastung*1000" >all.csv
+
+while [ "$BRUTTO" -lt "$MAX" ]; do {
+	BRUTTO=$(( BRUTTO + STEP ))
+
+	if   [ $BRUTTO -le 10908 ]; then
+		STEUER=0
+		NETTO=$BRUTTO
+		PERCENT=0
+	elif [ $BRUTTO -le 15999 ]; then	# 10.909 ... 15.999 Euro
+		Y="$( calc "($BRUTTO - 10908) / 10000" exact )"
+		STEUER="$( calc "((979.18*$Y)+1400)*$Y" )"
+	elif [ $BRUTTO -le 62809 ]; then	# 16.000 ... 62.809 Euro
+		Y="$( calc "($BRUTTO - 15999) / 10000" exact )"
+		STEUER="$( calc "((192.59*$Y)+2397)*$Y + 966.53" )"
+	elif [ $BRUTTO -le 277825 ]; then	# 62.810 ... 277.825 Euro
+		STEUER="$( calc "((0.42*$BRUTTO) - 9972.98)" )"
+	else
+		# >277.825 Euro
+		STEUER="$( calc "((0.45*$BRUTTO) - 18307.73)" )"
+	fi
+
+	[ $BRUTTO -gt 10908 ] && {
+		NETTO="$( calc "$BRUTTO - $STEUER" )"
+		PERCENT="$( calc "100-($NETTO*100/$BRUTTO)" )"
+	}
+
+	NETTO_MONTH="$( calc "$NETTO/12" )"
+	echo "Jahresbrutto: $BRUTTO Netto: $NETTO Steuer: $STEUER Prozent: $PERCENT (Monatsnetto: $NETTO_MONTH)"
+	echo "$BRUTTO $NETTO $STEUER $PERCENT" >>all.csv
+} done
+
+printf '%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n' \
+	"set object circle at first 83150,30000 radius char 0.5 fillstyle empty border lc rgb '#aa1100' lw 2" \
+	"set label 'Brutto 83150 € => 30% Steuern eff.' at 75000,32500" \
+	"set object circle at first 58450,25000 radius char 0.5 fillstyle empty border lc rgb '#aa1100' lw 2" \
+	"set label 'Brutto 58450 € => 25% Steuern eff.' at 48450,27500" \
+	"set ytics 2500" \
+	"set xtics 5000" \
+	"set term png" \
+	"set terminal png size 1900,1000" \
+	"set output 'all.png'" \
+	"set ylabel 'Ergebnis'" \
+	"set xlabel 'Bruttolohn in Euro im Jahr 2023'" \
+	"set title 'Steuerlast abhängig vom Brutto'" \
+	"set grid" \
+	"set key autotitle columnhead" \
+	"set key autotitle columnhead" \
+	"plot 'all.csv' using 1:2 with lines, '' using 1:3 with lines, '' using 1:(\$4*1000) with lines, '' using 1:1 with lines" | gnuplot
+
+# printf '%s\n%s\n%s\n%s\n%s\n%s\n%s\n' "set format y ''" "set xlabel 'Bruttolohn in Euro im Jahr 2023'" "set title 'Steuerlast abhängig vom Brutto'" "set grid" "set key autotitle columnhead" "set key autotitle columnhead" "plot 'all.csv' using 1:2 with lines, '' using 1:3 with lines, '' using 1:(\$4*1000) with lines, '' using 1:1 with lines" | gnuplot --persist
