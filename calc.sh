99f71cc0f6cd054a0f3d46b80012e072dcead2ed
Bastian Bittorf <bb@npl.de>
2024-01-14 17:36:21 +0100

    aufräumen und vorbereitung für 2024, mehr Text, mehr CSV, und Berücksichtigung Geringverdiener mit komischer Zacke

--- calc.sh
+++ calc.sh
@@ -12,10 +12,9 @@ STEP="${3:-100}"	# z.b. 25 Euro Schritte
 FILE_CSV='all.csv'
 FILE_PNG='all.png'
 
-YEAR=2023
-BRUTTO=0
-STEUER=0	# Lohnsteuer
+BRUTTO=${START:-0}	# enforce via ENV
 NETTO=0
+STEUER=0	# Lohnsteuer
 SOZIAL=0
 
 calc()
@@ -28,65 +27,95 @@ calc()
 
 calc_steuer2023()	# input: BRUTTO => emits variables: NETTO + STEUER + PERCENT
 {
-	if   [ $BRUTTO -le 10908 ]; then
+	if   [ "$BRUTTO" -le 10908 ]; then
 		STEUER=0
 		NETTO=$BRUTTO
 		PERCENT=0
-	elif [ $BRUTTO -le 15999 ]; then	# 10.909 ... 15.999 Euro
+	elif [ "$BRUTTO" -le 15999 ]; then	# 10.909 ... 15.999 Euro
 		Y="$( calc "($BRUTTO - 10908) / 10000" exact )"
 		STEUER="$( calc "((979.18*$Y)+1400)*$Y" )"
-	elif [ $BRUTTO -le 62809 ]; then	# 16.000 ... 62.809 Euro
+	elif [ "$BRUTTO" -le 62809 ]; then	# 16.000 ... 62.809 Euro
 		Y="$( calc "($BRUTTO - 15999) / 10000" exact )"
 		STEUER="$( calc "((192.59*$Y)+2397)*$Y + 966.53" )"
-	elif [ $BRUTTO -le 277825 ]; then	# 62.810 ... 277.825 Euro
+	elif [ "$BRUTTO" -le 277825 ]; then	# 62.810 ... 277.825 Euro
 		STEUER="$( calc "((0.42*$BRUTTO) - 9972.98)" )"
 	else
 		# >277.825 Euro
 		STEUER="$( calc "((0.45*$BRUTTO) - 18307.73)" )"
 	fi
 
-	# Grenzen:
-	# https://www.lohn-info.de/beitragsbemessungsgrenze_2023.html
-	#
-	# Pflegeversicherung/PV: 2023 = 3.4% mit Kindern (Arbeitnehmer 50% davon)
-	# Arbeitslosenversicherung/ALV: 2023 = 2.6% (Arbeitnehmer 50% davon)
-	# Deutsche Rentenversicherung/DRV: 2023 = 18.6% (Arbeitnehmer 50% davon)
-	# gesetzliche Krankenversicherung/GKV: 2023 = ~15% (Arbeitnehmer 50% davon)
-	# gesetzliche Unfallversicherung/GUV: 2023 = 0% (100% trägt Arbeitgeber)
-	PV="3.4/2"  && BRUTTO_PV="$BRUTTO" && test $BRUTTO -gt 59850 && BRUTTO_PV=59850		# = 4987,50 Euro/Monat
-	AV="2.6/2"  && BRUTTO_AV="$BRUTTO" && test $BRUTTO -gt 87600 && BRUTTO_AV=87600		# = 7300,00 Euro/Monat
-	RV="18.6/2" && BRUTTO_RV="$BRUTTO" && test $BRUTTO -gt 87600 && BRUTTO_RV=87600		# = 7300,00 Euro/Monat
-	KV="15/2"   && BRUTTO_KV="$BRUTTO" && test $BRUTTO -gt 59850 && BRUTTO_KV=59850		# = 4987,50 Euro/Monat
-	UV=0
-	SOZIAL="$( calc "($BRUTTO_PV/100*$PV) + ($BRUTTO_AV/100*$AV) + ($BRUTTO_RV/100*$RV) + ($BRUTTO_KV/100*$KV) + $UV" exact )"
+	if [ "$BRUTTO" -le $(( 325 * 12 )) ]; then
+		# Geringverdiener: bis 325 Euro zahlt Arbeitgeber Sozialversicherung allein
+		PV=0
+		AV=0
+		RV=0
+		KV=0
+	else
+		# Grenzen:
+		# https://www.lohn-info.de/beitragsbemessungsgrenze_2023.html
+		# Pflegeversicherung/PV: 2023 = 3.4% mit Kindern (Arbeitnehmer 50% davon)
+		PV="3.4/2" && BRUTTO_PV="$BRUTTO" && test "$BRUTTO" -gt 59850 && BRUTTO_PV=59850		# = 4987,50 Euro/Monat
+		PV="$( calc "($BRUTTO_PV/100)*$PV" )"
 
-	[ $BRUTTO -gt 10908 ] && {
+		# Arbeitslosenversicherung/ALV: 2023 = 2.6% (Arbeitnehmer 50% davon)
+		AV="2.6/2"  && BRUTTO_AV="$BRUTTO" && test "$BRUTTO" -gt 87600 && BRUTTO_AV=87600		# = 7300,00 Euro/Monat
+		AV="$( calc "($BRUTTO_AV/100)*$AV" )"
+
+		# Deutsche Rentenversicherung/DRV: 2023 = 18.6% (Arbeitnehmer 50% davon)
+		RV="18.6/2" && BRUTTO_RV="$BRUTTO" && test "$BRUTTO" -gt 87600 && BRUTTO_RV=87600		# = 7300,00 Euro/Monat
+		RV="$( calc "($BRUTTO_RV/100)*$RV" )"
+
+		# gesetzliche Krankenversicherung/GKV: 2023 = ~15% (Arbeitnehmer 50% davon)
+		# billigste war: BKK firmus und die BKK GILDEMEISTER SEIDENSTICKER mit 0.90% ZUsatzbeitrag = 15.5%
+		# FIXME! zwischen 520,01...2000 Euro monatlich (Midijob?) Übergangsrechnung/Gleitzone => Arbeitgeber zahlt mehr Anteil
+		KV="15.5/2" && BRUTTO_KV="$BRUTTO" && test "$BRUTTO" -gt 59850 && BRUTTO_KV=59850		# = 4987,50 Euro/Monat
+		KV="$( calc "($BRUTTO_KV/100)*$KV" )"
+	fi
+
+	# gesetzliche Unfallversicherung/GUV: 2023 = 0% (100% trägt Arbeitgeber)
+	UV=0
+
+	SOZIAL="$( calc "$PV + $AV + $RV + $KV + $UV" exact )"
+
+	[ "$BRUTTO" -gt 10908 ] && {
 		NETTO="$( calc "$BRUTTO - $STEUER" )"
 		PERCENT="$( calc "100-($NETTO*100/$BRUTTO)" )"
 	}
+
+	export BRUTTO NETTO STEUER SOZIAL PV AV RV KV PERCENT
 }
 
-CSV_HEADER="Brutto Netto Lohnsteuer Sozialabgaben effektive-prozentuale-Lohnsteuerbelastung*1000"
+CSV_HEADER="Brutto Lohnsteuer Netto Sozialabgaben Pflegevers. Arbeitslosenvers. Rentenvers. Krankenvers. Realnetto effektive-prozentuale-Lohnsteuerbelastung*1000"
 echo "$CSV_HEADER" >"$FILE_CSV"
 
-while [ "$BRUTTO" -lt "$MAX" ]; do {
-	BRUTTO=$(( BRUTTO + STEP ))
-	calc_steuer$YEAR "$BRUTTO"
+while [ "$BRUTTO" -le "$MAX" ]; do {
+	"calc_steuer${YEAR}" "$BRUTTO"
 
-	NETTO_MONTH="$( calc "$NETTO/12" )"
+	BRUTTO_MONTH="$( calc "$BRUTTO/12" )"
+	REALNETTO="$( calc "($NETTO-$SOZIAL)/1" )"
 	REALNETTO_MONTH="$( calc "($NETTO-$SOZIAL)/12" )"
 	SOZIAL="$( calc "$SOZIAL / 1" )"
-	echo "Jahresbrutto: $BRUTTO Netto: $NETTO Steuer: $STEUER Sozial: $SOZIAL Prozent: $PERCENT (Monatsnetto/real: $NETTO_MONTH / $REALNETTO_MONTH)"
-	echo "$BRUTTO $NETTO $STEUER $SOZIAL $PERCENT" >>"$FILE_CSV"
+
+	HOUR_BRUTTO="$( calc "$BRUTTO/(260*8)" )"
+	HOUR_REALNETTO="$( calc "$REALNETTO/(260*8)" )"
+
+	echo "Brutto_Jahr/Monat/Stunde: $BRUTTO / $BRUTTO_MONTH / $HOUR_BRUTTO Lohnsteuer%: $PERCENT Lohnsteuer: $STEUER Netto: $NETTO Sozial: $SOZIAL PV: $PV AV: $AV RV: $RV KV: $KV Realnetto_Jahr/Monat/Stunde $REALNETTO / $REALNETTO_MONTH / $HOUR_REALNETTO"
+	echo "$BRUTTO $STEUER $NETTO $SOZIAL $PV $AV $RV $KV $REALNETTO $PERCENT" >>"$FILE_CSV"
+
+	BRUTTO=$(( BRUTTO + STEP ))
 } done
 
-# Realnetto (Lohnsteuer+Sozialabgabe):
-# Brutto: 83109 => Realnetto 41720.62 => 3476.72 monatlich
-# Brutto: 66666 => Realnetto 35439.40 => 2953.28 monatlich
+# Beispiele Realnetto (Brutto minus Lohnsteuer und Sozialabgaben):
+# Brutto: 24960 = Mindestlohn 2023 = 12 Euro * 8h * 260 Tage
+# Brutto_Jahr/Monat/Stunde: 24960 / 2080.00 / 12.00 Lohnsteuer%: 13.09 Lohnsteuer: 3269.13 Netto: 21690.87 Sozial: 5004.48 PV: 424.32 AV: 324.48 RV: 2321.28 KV: 1934.40 Realnetto_Jahr/Monat/Stunde 16686.39 / 1390.53 / 8.02
+# Brutto_Jahr/Monat/Stunde: 33333 / 2777.75 / 16.02 Lohnsteuer%: 17.10 Lohnsteuer: 5700.16 Netto: 27632.84 Sozial: 6683.24 PV: 566.66 AV: 433.32 RV: 3099.96 KV: 2583.30 Realnetto_Jahr/Monat/Stunde 20949.60 / 1745.80 / 10.07
+# Brutto_Jahr/Monat/Stunde: 66666 / 5555.50 / 32.05 Lohnsteuer%: 27.04 Lohnsteuer: 18026.74 Netto: 48639.26 Sozial: 12722.40 PV: 1017.45 AV: 866.65 RV: 6199.93 KV: 4638.37 Realnetto_Jahr/Monat/Stunde 35916.86 / 2993.07 / 17.26
+#
+# Beispiel-A:
 R0=36066;R0X=66666;R0Y=$R0;J0X=$((R0X-22500));J0Y=$((R0Y+2500));R0HUMAN="$( calc "scale=3;$R0X/1000" exact )";M="$( calc "$R0/12" )"
-RP="$( calc "100-(($R0*100)/$R0X)" )";R0H="$( calc "$R0X/(260*8)" )";MM="$( calc "$R0/(260*8)" )";R0=36.066
+RP="$( calc "100-(($R0*100)/$R0X)" )";R0H="$( calc "$R0X/(260*8)" )";MM="$( calc "$R0/(260*8)" )";R0="$( calc "scale=3;$R0/1000" exact )"
 
-# Grenzen Soizialversicherung:
+# Grenzen Sozialversicherung:
 GRENZE_X1=59850 && GRENZE_Y1=11850 && HGRENZE_X1="$( calc "scale=3;$GRENZE_X1/1000" exact )"	# PV/KV
 GRENZE_X2=87600 && GRENZE_Y2=14792 && HGRENZE_X2="$( calc "scale=3;$GRENZE_X2/1000" exact )"	# AV/RV
 
@@ -131,12 +160,12 @@ printf '%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n
 	"set output '$FILE_PNG'" \
 	"set ylabel 'Ergebnis'" \
 	"set xlabel 'Jahresbruttolohn in Euro im Jahr $YEAR'" \
-	"set title 'Jährliche Abgaben- und Steuerlast abhängig vom Brutto (verheiratet, 2 Kinder, alte Bundesländer)'" \
+	"set title 'Jährliche Abgaben- und Steuerlast für Arbeitnehmer abhängig vom Brutto (verheiratet, 2 Kinder, alte Bundesländer)'" \
 	"set grid" \
 	"set key autotitle columnhead" \
-	"plot '$FILE_CSV'   using 1:2 with lines, \\" \
-			"'' using 1:3 with lines, \\" \
+	"plot '$FILE_CSV'   using 1:3 with lines, \\" \
+			"'' using 1:2 with lines, \\" \
 			"'' using 1:4 with lines, \\" \
-			"'' using 1:(\$5*1000) with lines, \\" \
-			"'' using 1:(\$2-\$4) title 'Realnetto (Brutto abzüglich Lohnsteuer+Sozialabgaben)' with lines, \\" \
+			"'' using 1:(\$10*1000) with lines, \\" \
+			"'' using 1:(\$1-\$2-\$4) title 'Realnetto (Brutto abzüglich Lohnsteuer und Sozialabgaben)' with lines, \\" \
 			"'' using 1:1 with lines" | gnuplot && echo "see '$FILE_CSV' and '$FILE_PNG'"
